f78c235d47acd7a067148dbcc32eb8007dafd086
paranoidandroid13 <utf1313@gmail.com>
2021-06-12 19:39:27 +0300

    posts context

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState, useContext, createContext } from 'react'
 import axios from 'axios'
 import { AuthContext } from '../contexts/AuthContext'
 import ProductList from '../components/ProductList'
 
-function Products(props) {
-  // TODO: контекст возвращает только юзернейм
-  // TODO: приватных постов не сущетсвует
-  const userContext = useContext(AuthContext)
-  const isLogged = false
+export const ProductsContext = createContext()
+
+export const ProductsProvider = (props) => {
   const [products, setProducts] = useState([])
+  
   useEffect(() => {
     const initProducts = async () => {
       const { data } = await axios(`https://mockapi.42.works/api/posts`)
       const publicProducts = data.data.filter((item) => {
         return !item.is_private
       })
-      setProducts(isLogged ? data.data : publicProducts)
+      setProducts(publicProducts)
     }
-
     initProducts()
+    console.log('page', products)
   }, [])
   return (
-    <ProductList products={products} />
+    <ProductsContext.Provider value={[products, setProducts]}> 
+      <ProductList />
+    </ProductsContext.Provider>
   )
 }
-
-export default Products
