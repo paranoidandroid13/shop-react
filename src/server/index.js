f21eea67d5950ad9c8696ba2a3b342302d55f9cd
paranoidandroid13 <utf1313@gmail.com>
2021-06-17 13:21:49 +0300

    expressjs

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,33 @@
 const express = require('express')
 const { readFile, appendFile, writeFile, stat, unlink } = require('fs').promises
 const path = require('path')
+const axios = require('axios')
+const circularJSON = require('circular-json')
 
 const server = express()
 
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 
-server.post('/api/v1/auth', (req, res) => {
-  res.json({ isLogged: true })
-})
+// server.post('/api/v1/auth', (req, res) => {
+//   res.json({ isLogged: true })
+// })
 
 server.get('/api/v1/posts', async (req, res) => {
-  const { data } = await `https://mockapi.42.works/api/posts`
   const pathToData = path.join(__dirname, '/data/posts.json')
-  const file = await writeFile(pathToData, data, { encoding: 'utf8' })
-  res.json(file)
+  const data = await axios(`https://jsonplaceholder.typicode.com/users`)
+  await writeFile(pathToData, circularJSON.stringify(data.data), { encoding: 'utf8' })
+  res.send(circularJSON.stringify(data.data))
 })
 
-server.listen(3004, () => {
-  const pathToData = path.join(__dirname, '/data/posts.json')
+// server.get('/api/v1/posts', (req, res) => {
+//   readFile(`${__dirname}/data/posts.json`, { encoding: 'utf8' })
+//     .then((posts) => res.send(posts))
+//     .catch(async () => {
+//       const posts = await axios(`https://jsonplaceholder.typicode.com/users`).then((data) => data)
+//       writeFile(`${__dirname}/data/posts.json`, JSON.stringify(posts), { encoding: 'utf8' })
+//       res.send(posts)
+//     })
+// })
 
-  console.log(pathToData)
-})
+server.listen(3004, () => {})
